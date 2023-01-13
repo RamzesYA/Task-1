ebe99e633e2b2614ef80792b71a34f9db2d63c01
RamzesYA <roma.efremov.2002@gmail.com>
2023-01-13 13:03:20 +0300

    Modified main.py
    Added call to required functions

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from info import key
+from func import *
+
+
 if __name__ == '__main__':
-    pass
+    name = input('Input city in english: ')
+    get_url(name, key)
+    base_data()
+    get_city_temp()
+    get_city_wind()
+
