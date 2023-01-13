b3c9baf12edf05e25fa32cb0379c26ae59cbad68
RamzesYA <roma.efremov.2002@gmail.com>
2023-01-13 13:01:52 +0300

    Modified func.py
    Added base_data(), get_city_temp(), get_city_wind

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 def output(dict):
     for key, value in dict.items():
@@ -13,3 +14,21 @@ def get_url(name, key):
     if updates['cod']!=200:
         print('Wrong city')
         exit()
+
+def base_data():
+    date = datetime.fromtimestamp(updates['dt']).strftime('%d-%m-%Y %H:%M:%S')
+    print(f'\n{updates["name"]}\n{date}\n')
+
+def get_city_temp():
+    answer = {}
+
+    answer['Temperature'] = updates['main']['temp']
+    answer['Feels like'] = updates['main']['feels_like']
+    output(answer)
+
+def get_city_wind():
+    answer = {}
+
+    answer['Wind speed'] = updates['wind']['speed']
+    answer['Gusts to'] = updates['wind']['gust']
+    output(answer)
